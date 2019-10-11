620ff147d570e581e061fe31eaf89740a30abd1c
rnhappysmile <rnhappysmile@gmail.com>
2019-10-12 02:45:40 +0900

    - 현재 날자의 달력 표현기능 구현

--- calendar.js
+++ calendar.js
@@ -1,95 +1,94 @@
 const MomentumCalendar = document.querySelector(".js-calendar")
+const calBody = document.querySelector(".cal-body")
 
-function init(){
-// 3가지 달력을 그려줘야 함
-    var now = new Date();
-    now.year
-    now.month
-    now.days_in_month
+const initDate = {
+  monList: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  dayList: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  today: new Date(),
+  monForChange: new Date().getMonth(),
+  activeDate: new Date(),
+  getFirstDay: (yy, mm) => new Date(yy, mm, 1),
+  getLastDay: (yy, mm) => new Date(yy, mm + 1, 0),
+  nextMonth: function() {
+    let d = new Date();
+    d.setDate(1);
+    d.setMonth(++this.monForChange);
+    this.activeDate = d;
+    return d;
+  },
+  prevMonth: function () {
+    let d = new Date();
+    d.setDate(1);
+    d.setMonth(--this.monForChange);
+    this.activeDate = d;
+    return d;
+  },
+  addZero: (num) => (num < 10) ? '0' + num : num,
+  activeDTag: null,
+  getIndex: function (node) {
+    let index = 0;
+    while (node = node.previousElementSibling) {
+      index++;
+    }
+    return index;
+  }
+};
+
+function setCalHead (month, fullYear) {
+  document.querySelector('.cal-month').textContent = initDate.monList[month];
+  document.querySelector('.cal-year').textContent = fullYear;
+}
+
+
+/**
+* @param {date} fullDate
+*/
+
+function loadYYMM (fullDate) {
+  let yy = fullDate.getFullYear();
+  let mm = fullDate.getMonth();
+  let firstDay = initDate.getFirstDay(yy, mm);
+  let lastDay = initDate.getLastDay(yy, mm);
+  let markToday; // for marking today date
+
+  if (mm === initDate.today.getMonth() && yy === initDate.today.getFullYear()){
+    markToday = initDate.today.getDate();
+  }
+
+  setCalHead(mm, yy);
+
+  let trtd = '';
+  let startCount;
+  let countDay = 0;
+  for (let i = 0; i < 6; i++) {
+    trtd += '<tr>';
+    for (let j = 0; j < 7; j ++) {
+      if (i === 0 && !startCount && j === firstDay.getDay()){
+        startCount = 1;
+      }
+
+      if(!startCount){
+        trtd += '<td>'
+      } else {
+        let fullDate = yy + '.' + initDate.addZero(mm + 1) + '.' + initDate.addZero(countDay + 1);
+        trtd += '<td class="day';
+        trtd += (markToday && markToday === countDay + 1) ? ' today" ':'"';
+        trtd += ' data-date="${countDay + 1}" data-fdate="&{fullDate}">';
+      }
+      trtd += (startCount) ? ++countDay : '';
+      if(countDay === lastDay.getDate()){
+        startCount = 0;
+      }
+      trtd += '</td>';
+    }
+    trtd += '</td>';
+  }
+  calBody.innerHTML = trtd;
+}
+
+function init(){    
+    setCalHead(initDate.today.getMonth(), initDate.today.getFullYear());
+    loadYYMM(initDate.today);
 }
 
 init();
-
-
-
-
-
-// to see a calendar with weeks starts by Monday look here: https://codepen.io/jacknumber/pen/RWLyQW
-
-function calendar(params) {
-    var days_labels = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'],
-        months_labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    
-    var days_in_month = getDaysInMonth(params.month, params.year),
-        first_day_date = new Date(params.year, params.month, 1),
-        first_day_weekday = first_day_date.getDay();
-    
-    var prev_month = params.month == 0 ? 11 : params.month - 1,
-        prev_year = prev_month == 11 ? params.year - 1 : params.year,
-        prev_days = getDaysInMonth(prev_month, prev_year);
-    
-    // calendar header
-    var html = '<h2>' + months_labels[params.month] + ' ' + params.year + '</h2>';
-    
-    function getDaysInMonth(month, year) {
-      // 0 = last day of the previous month
-      return new Date(year, month + 1, 0).getDate();
-    }
-    
-    // calendar content
-    html += '<table class="calendar-table">';
-    
-    // week days labels
-    html += '<tr class="week-days">';
-    for (var i = 0; i <= 6; i++) {
-      html += '<td class="day">';
-      html += days_labels[i];
-      html += '</td>';
-    }
-    html += '</tr>';
-    
-    var w = 0; // week day
-    var n = 1; // next days date
-    var c = 1; // current date
-    
-    // dates loop
-    for (var i = 0; i < 6*days_labels.length; i++) {
-      if (w == 0) {
-        // first week's day
-        html += '<tr class="week">';
-      }    
-      
-      if (i < new Date(params.year, params.month, 1).getDay()) {
-        // previous month's day
-        html += '<td class="day other-month">' + (prev_days - first_day_weekday + i + 1) + '</td>';
-      } else if (c > days_in_month) {
-        // next month's day
-        html += '<td class="day other-month">' + n + '</td>';
-        n++;
-      } else {
-        // current month's day
-        var options = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
-        var display_date = new Date(params.year, params.month, c);
-        html += '<td class="day" title="' + display_date.toLocaleDateString('en-GB', options) + '">' + c + '</td>';
-        c++;
-      }
-      
-      if (w == days_labels.length - 1) {
-        // last week's day
-        html += '</tr>';
-        w = 0;
-      } else {
-        w++;
-      }
-    }  
-  
-    html += '</tr>'; 
-    return html;
-  }
-  
-  var now = new Date();
-  var params = {
-    month: now.getMonth(),
-    year: now.getFullYear()
-  };
-  document.getElementById('calendar').innerHTML = calendar(params);
